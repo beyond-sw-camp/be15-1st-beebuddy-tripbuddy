70f968d66d66eb19688cee66302907f0dae75e16
Cho-Hyun-Seung <toki0327@naver.com>
2025-02-12 01:09:41 +0900

    FEAT: 예산 쿼리 작성 중
    - 예산 쿼리 작성
    - 공용 프로시저 작성
    - 멤버 더미데이터 추가

--- src/sql/budget/budget.sql
+++ src/sql/budget/budget.sql
@@ -1,36 +1,100 @@
 -- 예산 DML
 
--- 1. 예산 조회
+#### 백엔드로부터 받은 내용 ####
+SET @member_id = 202;
+SET @group_id = 1;
+SET @room_id = 12;
+SET @category_id = 2;
+-- 0. 공통 사항
+-- (1) 권한 확인
+CALL proc_isMemberExist(@member_id); -- 유저 존재 확인
+CALL proc_isMemberInGroup(@member_id, @group_id); -- 그룹에 속한지 확인
+
+#### 1. 예산 조회 ####
+-- default 정렬	: 수정시간 오름차
 
 -- 1.1. 총 예산 조회
-
--- (1) 조회하려는 유저가 활성화 상태인지 확인
-
+-- 조회 내용 : 예산 총합
 SELECT
 	SUM(budget_amount)
 FROM
 	tbl_budget
-JOIN
-	tbl_member	mb
-ON
-	mb
+WHERE room_id = @room_id;
 
 
 
 -- 1.2. 예산 목록 조회
+-- 조회 내용	: 예산 ID, 예산 제목, 예산 금액, 카테고리명
+-- (1) 모두 보기
+SELECT
+	  bg.budget_id
+	, bg.budget_name
+	, bg.budget_amount
+	, cg.category_name
+FROM
+	tbl_budget
+JOIN
+	tbl_category cg
+ON
+	bg.category_id = cg.category_id
+WHERE
+	room_id = @room_id
+ORDER BY
+	updated_at ASC;	
+
+-- (2) 카테고리 별 보기
+SELECT
+	  bg.budget_id
+	, bg.budget_name
+	, bg.budget_amount
+	, cg.category_name
+FROM
+	tbl_budget bg
+JOIN
+	tbl_category cg
+ON
+	bg.category_id = cg.category_id
+WHERE
+	room_id = @room_id
+AND
+	category_id = @category_id -- 실제로는 in 형태로 동작할 수 있음
+ORDER BY
+	updated_at ASC;	
 
 
 -- 1.3. 예산 상세 조회
+-- 조회 목록: id, 제목, 금액, 작성자, 카테고리명, 수정 시간
+SELECT
+	  bg.budget_id
+	, bg.budget_name
+	, bg.budget_amount
+	, mb.member_name
+	, cg.category_name
+	, bg.updated_at
+FROM
+	tbl_budget bg
+JOIN
+	tbl_category cg
+ON
+	bg.category_id = cg.category_id
+JOIN
+	tbl_member mb
+ON
+	bg.writer_id = mb.member_id
+WHERE
+	room_id = @room_id
+ORDER BY
+	updated_at ASC;
 
 
--- 2. 예산 작성
+#### 2. 예산 작성 ####
 
--- 3. 예산 수정
+#### 3. 예산 수정 ####
 
--- 4. 예산 삭제
+#### 4. 예산 삭제 ####
 
--- 5. 예산 댓글 조회
+#### 5. 예산 댓글 조회 ####
 
--- 6. 예산 댓글 수정
+#### 6. 예산 댓글 수정 ####
 
--- 7. 예산 댓글 삭제
+#### 7. 예산 댓글 삭제 ####
---
+++ src/sql/budget/proc.sql
@@ -0,0 +1,71 @@
+# 회원 존재 여부 확인
+-- 아이디가 테이블에 없는 경우
+-- 있어도 is_deleted = 'Y'인 경우
+DELIMITER //
+
+CREATE OR REPLACE PROCEDURE proc_isMemberExist(
+	IN	target_member_id INT
+)
+BEGIN
+	DECLARE member_not_found CONDITION FOR SQLSTATE '45000';
+	DECLARE count_member INT;
+	
+	DECLARE EXIT HANDLER FOR member_not_found
+	BEGIN
+		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = '존재하지 않는 유저입니다.';
+	END;
+	
+	SELECT
+		COUNT(*) INTO count_member
+	FROM 
+		tbl_member
+	WHERE member_id = target_member_id AND is_deleted = 'N';
+	
+	IF count_member = 0 THEN
+		SIGNAL member_not_found;
+	END IF;
+
+END //
+
+DELIMITER ;
+
+ #테스트용
+ -- CALL proc_isMemberExist(999);
+ 
+# 회원 그룹원 여부 확인
+-- pk를 통해 존재 확인
+-- 존재한다면 초대상태가 'y'여야 함
+
+DELIMITER //
+
+CREATE OR REPLACE PROCEDURE proc_isMemberInGroup(
+	IN	target_member_id INT,
+	IN target_group_id INT
+)
+BEGIN
+	DECLARE member_not_in_group CONDITION FOR SQLSTATE '45000';
+	DECLARE count_member INT;
+	
+	DECLARE EXIT HANDLER FOR member_not_in_group
+	BEGIN
+		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = '그룹에 접근할 수 없습니다.';
+	END;
+	
+	SELECT
+		COUNT(*) INTO count_member
+	FROM 
+		tbl_group_member
+	WHERE member_id = target_member_id 
+	AND group_id = target_group_id -- pk용
+	AND invite_status = 'Y'; -- 초대에 응한 상태일 경우
+	
+	IF count_member = 0 THEN
+		SIGNAL member_not_in_group;
+	END IF;
+	
+END // 
+
+DELIMITER ;
+ 
+ 
+ 
