360d60e9c63af14ea89033c6a1ad51066e5e4b93
sowon <nare20027@gmail.com>
2025-02-13 00:05:29 +0900

    feat: 001 알림 조회, 확인, 삭제
    6개월 이내의 알림 조회,  미확인 알림 확인, 알림 삭제

---
+++ src/sql/notice/notice.sql
@@ -0,0 +1,23 @@
+-- 1. 회원 알림 조회
+SELECT
+      mnt.member_notice_id,
+      nt.notice_name,
+      mnt.check_status,
+      mnt.notice_time
+  FROM tbl_member_notice mnt
+  JOIN tbl_notice nt ON mnt.notice_id = nt.notice_id 
+ WHERE member_id = 37 
+   AND 6 >= TIMESTAMPDIFF(month, mnt.notice_time, CURRENT_TIMESTAMP);
+
+-- 2. 회원 알림 확인
+UPDATE tbl_member_notice
+   SET check_status = 'Y'
+ WHERE member_id = 37 
+   AND member_notice_id = 3;  
+
+
+-- 3. 회원 조회 알림 삭제 
+DELETE 
+  FROM tbl_member_notice
+ WHERE member_id = 37 
+   AND member_notice_id = 1; 
