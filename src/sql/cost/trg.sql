e23adbe55465b45645428744ec35f3cec482b306
Cho-Hyun-Seung <toki0327@naver.com>
2025-02-13 11:41:42 +0900

    fix: 비용 UPDATE 트리거 수정
    - 비용 업데이트 트리거가 after delete로 되어 있던 오류 수정
    #14

--- src/sql/cost/trg.sql
+++ src/sql/cost/trg.sql
@@ -37,8 +37,10 @@ DELIMITER ;
 
 
 -- 비용 수정 트리거
+DELIMITER //
+
 CREATE OR REPLACE TRIGGER after_update_cost
-	AFTER DELETE
+	AFTER UPDATE
 	ON tbl_cost
 	FOR EACH ROW
 BEGIN
@@ -73,7 +75,11 @@ END //
 
 DELIMITER ;
 
+
 -- 비용 삭제 트리거
+
+DELIMITER //
+
 CREATE OR REPLACE TRIGGER after_delete_cost
 	AFTER DELETE
 	ON tbl_cost
@@ -95,13 +101,13 @@ BEGIN
 	VALUES
 	(
 		'DELETED'
-		, NEW.created_at
-		, NEW.cost_amount
-		, NEW.cost_name
-		, NEW.writer_id
-		, NEW.category_id
-		, NEW.payer_id
-		, NEW.room_id
+		, created_at
+		, cost_amount
+		, cost_name
+		, writer_id
+		, category_id
+		, payer_id
+		, room_id
 		, NULL -- cost_id null로 바꿔줌!
 	);
 	
