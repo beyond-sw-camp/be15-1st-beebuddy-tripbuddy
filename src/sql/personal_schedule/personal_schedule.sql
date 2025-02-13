44d16063b00c61e0c677bf5b88dbcc6b04d419ea
sowon <nare20027@gmail.com>
2025-02-13 12:19:35 +0900

    fix : tbl_personal_schdule 개인 일정 수정 #15
    개인 일정 수정 query 추가

--- src/sql/personal_schedule/personal_schedule.sql
+++ src/sql/personal_schedule/personal_schedule.sql
@@ -25,7 +25,26 @@ INSERT INTO tbl_personal_schedule(schedule_name, startdate, enddate, member_id)
 VALUES
 ('개인일정','2025-05-02',NULL,37);
 
--- 4. 개인 일정 삭제
+
+-- 4. 개인 일정 수정
+-- 4-1. 개인 일정 제목 수정
+UPDATE tbl_personal_schedule
+SET schedule_name = '개인일정2'
+WHERE ps_id = 6;
+-- 4-2. 개인 일정 날짜 수정
+UPDATE tbl_personal_schedule
+SET startdate = '2025-02-19', 
+    enddate = '2025-02-20'
+WHERE ps_id = 6;
+-- 4-3. 전체 수정
+UPDATE tbl_personal_schedule
+SET schedule_name = '개인일정2',
+    startdate = '2025-02-19', 
+    enddate = '2025-02-20'
+WHERE ps_id = 6;
+
+
+-- 5. 개인 일정 삭제
 DELETE 
   FROM tbl_personal_schedule
  WHERE member_id = 37 -- 멤버아이디
