20417accc324b0c564cb49783ecb35c3ca9b8e66
Cho-Hyun-Seung <toki0327@naver.com>
2025-02-13 12:31:25 +0900

    fix: 트리거 명명 규칙 지키기
    trg_트리거명으로 수정 #14

---
+++ src/sql/cost/trg.sql
@@ -0,0 +1,117 @@
+-- 비용 생성 트리거
+DELIMITER //
+
+CREATE OR REPLACE TRIGGER trg_after_insert_cost
+	AFTER INSERT
+	ON tbl_cost
+	FOR EACH ROW
+BEGIN
+	INSERT INTO
+		tbl_cost_history
+	(
+		history_type
+		, updated_at
+		, cost_amount
+		, cost_name
+		, writer_id
+		, category_id
+		, payer_id
+		, room_id
+		, cost_id
+	)
+	VALUES
+	(
+		'CREATED'
+		, NEW.created_at
+		, NEW.cost_amount
+		, NEW.cost_name
+		, NEW.writer_id
+		, NEW.category_id
+		, NEW.payer_id
+		, NEW.room_id
+		, NEW.cost_id
+	);
+END //
+
+DELIMITER ;
+
+
+-- 비용 수정 트리거
+DELIMITER //
+
+CREATE OR REPLACE TRIGGER trg_after_update_cost
+	AFTER UPDATE
+	ON tbl_cost
+	FOR EACH ROW
+BEGIN
+	INSERT INTO
+		tbl_cost_history
+	(
+		history_type
+		, updated_at
+		, cost_amount
+		, cost_name
+		, writer_id
+		, category_id
+		, payer_id
+		, room_id
+		, cost_id
+	)
+	VALUES
+	(
+		'UPDATED'
+		, NEW.created_at
+		, NEW.cost_amount
+		, NEW.cost_name
+		, NEW.writer_id
+		, NEW.category_id
+		, NEW.payer_id
+		, NEW.room_id
+		, NEW.cost_id
+	);
+	
+
+END //
+
+DELIMITER ;
+
+
+-- 비용 삭제 트리거
+
+DELIMITER //
+
+CREATE OR REPLACE TRIGGER trg_after_delete_cost
+	AFTER DELETE
+	ON tbl_cost
+	FOR EACH ROW
+BEGIN
+	INSERT INTO
+		tbl_cost_history
+	(
+		history_type
+		, updated_at
+		, cost_amount
+		, cost_name
+		, writer_id
+		, category_id
+		, payer_id
+		, room_id
+		, cost_id
+	)
+	VALUES
+	(
+		'DELETED'
+		, created_at
+		, cost_amount
+		, cost_name
+		, writer_id
+		, category_id
+		, payer_id
+		, room_id
+		, NULL -- cost_id null로 바꿔줌!
+	);
+	
+
+END //
+
+DELIMITER ;
